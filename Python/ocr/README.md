733e9f414289d606e11045a9a91ae37d8bafefeb
cloutlee <clout_lee@hotmail.com.tw>
2021-09-28 15:48:27 +0800

    add

--- Python/ocr/README.md
+++
@@ -1,7 +0,0 @@
-# PyTesseract
-
-步驟:
-for windows
-1.[下載及安裝執行檔](https://github.com/UB-Mannheim/tesseract/wiki)
-2.設定環境變數
-3.安裝Python套件pip3 install pytesseract
--- Python/ocr/ocr.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-
-img = Image.open('12.png')
-img.show()
-text = pytesseract.image_to_string(img, lang="chi_tra+eng")
-print(text)
