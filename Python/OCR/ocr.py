5319791eee80eef07dac61acc2027bbde00e8f4c
cloutlee <clout_lee@hotmail.com.tw>
2021-09-29 15:17:58 +0800

    modify

--- Python/OCR/ocr.py
+++ Python/OCR/ocr.py
@@ -11,15 +11,15 @@ resize_img = cv2.resize(img, (300, 300), interpolation=cv2.INTER_CUBIC)
 gray_img = cv2.cvtColor(resize_img, cv2.COLOR_RGB2GRAY)
 
 #thresholding
-sim_img = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY_INV)[1]
+thres_img = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY_INV)[1]
 
 #blur
-mblur = cv2.medianBlur(sim_img, 3)
+blur_img = cv2.medianBlur(thres_img, 3)
 
 kernel = np.ones((2, 2), np.uint8)
-open_img = cv2.morphologyEx(mblur, cv2.MORPH_OPEN, kernel)
+morph_img = cv2.morphologyEx(blur_img, cv2.MORPH_OPEN, kernel)
 
-text = pytesseract.image_to_string(open_img, lang="chi_tra+eng")
+text = pytesseract.image_to_string(morph_img, lang="chi_tra+eng")
 print(text)
 cv2.imshow('imgage', img)
 cv2.waitKey(0)
