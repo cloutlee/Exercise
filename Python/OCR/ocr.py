6b35c15d2bc5a98af01becb24f29f3054635aabc
cloutlee <clout_lee@hotmail.com.tw>
2021-09-29 15:02:34 +0800

    modify

--- Python/OCR/ocr.py
+++ Python/OCR/ocr.py
@@ -1,7 +1,19 @@
 import pytesseract
-from PIL import Image
+import cv2
+import numpy as np
 
-img = Image.open('12.png')
-img.show()
-text = pytesseract.image_to_string(img, lang="chi_tra+eng")
+img = cv2.imread('12.png', cv2.IMREAD_COLOR)
+#縮放
+resize_img = cv2.resize(img, (300, 300), interpolation=cv2.INTER_CUBIC)
+#灰階
+gray_img = cv2.cvtColor(resize_img, cv2.COLOR_RGB2GRAY)
+#二值化反轉
+sim_img = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY_INV)[1]
+#模糊
+mblur = cv2.medianBlur(sim_img, 3)
+kernel = np.ones((2, 2), np.uint8)
+open_img = cv2.morphologyEx(mblur, cv2.MORPH_OPEN, kernel)
+text = pytesseract.image_to_string(open_img, lang="chi_tra+eng")
 print(text)
+cv2.imshow('imgage', img)
+cv2.waitKey(0)
