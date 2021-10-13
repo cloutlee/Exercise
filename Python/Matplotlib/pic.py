07be888ad360723d2858a6ad7a6e03604bfdbcd4
cloutlee <clout_lee@hotmail.com.tw>
2021-10-13 15:52:11 +0800

    add

--- Python/Matplotlib/pic.py
+++ Python/Matplotlib/pic.py
@@ -1,6 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+x = [1, 2, 3, 4, 5]
+y = [12, 22, 15, 11, 18]
+plt.plot(x, y, color='red')
+plt.grid(color='0.9')
+plt.show()
+
+
+x1 = np.arange(-1, 6)
+y1 = 1/2 * x1 + 1/2
+y2 = -2 * x1 + 7
+plt.plot(x1, y1)
+plt.plot(x1, y2)
+plt.axis('equal')
+plt.grid()
+plt.show()
+
+
 values = [20, 59, 70]
 labels = ['A', 'B', 'C']
 plt.bar(labels, values)
@@ -24,3 +41,10 @@ plt.hist(ironman_hist, bins=10, color='#f19790', edgecolor="LightSteelBlue")
 plt.show()
 
 
+th = np.arange(0, 360)
+x3 = np.cos(np.radians(th))
+y3 = np.sin(np.radians(th))
+plt.plot(x3, y3)
+plt.axis('equal')
+plt.grid()
+plt.show()
