828b5b9438763b944451bd37b0b230f67cdaf0f3
cloutlee <clout_lee@hotmail.com.tw>
2025-11-01 17:51:28 +0800

    test

--- Python/numpy/test1.py
+++ Python/numpy/test1.py
@@ -2,6 +2,7 @@ import numpy as np
 
 tmplist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 arr = np.array(tmplist)
+
 # print(arr)
 # print(arr.shape)
 # print(arr.dtype)
@@ -9,17 +10,19 @@ arr = np.array(tmplist)
 # print(arr.ndim)
 # print(arr.size)
 
-# print(tmplist[:])       #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# print(tmplist[1:3])     #[2, 3]
-# print(tmplist[2:])      #[3, 4, 5, 6, 7, 8, 9, 10]
-# print(tmplist[:3])      #[1, 2, 3]      
-# print(tmplist[1:8:2])   #start:end:step [2, 4, 6, 8]
-# print(tmplist[-1])      #10
-# print(tmplist[-2])      #9
-# print(tmplist[-3:])     #[8, 9, 10]
-# print(tmplist[:-3])     #[1, 2, 3, 4, 5, 6, 7]
+# print(arr[:])       #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(arr[1:3])     #[2, 3]
+# print(arr[2:])      #[3, 4, 5, 6, 7, 8, 9, 10]
+# print(arr[:3])      #[1, 2, 3]      
+# print(arr[1:8:2])   #start:end:step [2, 4, 6, 8]
+# print(arr[-1])      #10
+# print(arr[-2])      #9
+# print(arr[-3:])     #[8, 9, 10]
+# print(arr[:-3])     #[1, 2, 3, 4, 5, 6, 7]
 
 print(np.zeros(3))        #[0. 0. 0.]
 print(np.zeros((2,3)))    #[[0. 0. 0.]
                           # [0. 0. 0.]]
 print(np.ones(3))         #[1. 1. 1.]
+
+
