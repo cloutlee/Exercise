8445cf85a6da97714f895647d1fce6f90d78c64c
cloutlee <clout_lee@hotmail.com.tw>
2025-11-01 17:43:29 +0800

    test

--- Python/numpy/test1.py
+++ Python/numpy/test1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-tmplist = [1, 2, 3, 4, 5]
+tmplist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 arr = np.array(tmplist)
 # print(arr)
 # print(arr.shape)
@@ -9,7 +9,17 @@ arr = np.array(tmplist)
 # print(arr.ndim)
 # print(arr.size)
 
-print(tmplist[:])
-print(tmplist[1:3])
-print(tmplist[2:])
-print(tmplist[:3])
+# print(tmplist[:])       #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(tmplist[1:3])     #[2, 3]
+# print(tmplist[2:])      #[3, 4, 5, 6, 7, 8, 9, 10]
+# print(tmplist[:3])      #[1, 2, 3]      
+# print(tmplist[1:8:2])   #start:end:step [2, 4, 6, 8]
+# print(tmplist[-1])      #10
+# print(tmplist[-2])      #9
+# print(tmplist[-3:])     #[8, 9, 10]
+# print(tmplist[:-3])     #[1, 2, 3, 4, 5, 6, 7]
+
+print(np.zeros(3))        #[0. 0. 0.]
+print(np.zeros((2,3)))    #[[0. 0. 0.]
+                          # [0. 0. 0.]]
+print(np.ones(3))         #[1. 1. 1.]
