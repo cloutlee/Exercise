92b55949c98af1cb0fea288ddbfbc44f9a0b1482
cloutlee <clout_lee@hotmail.com.tw>
2025-11-01 19:41:15 +0800

    test

--- Python/seaborn/test1.py
+++ Python/seaborn/test1.py
@@ -1,7 +1,14 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-data = np.random.randn(50).cumsum()
-sns.histplot(data=data)
+# data = np.random.randn(50).cumsum()
+# sns.histplot(data=data)
+# plt.show()
+
+x = np.linspace(1, 7, 50)
+y = 3 + 2 * x + 1.5 * np.random.randn(len(x))
+df = pd.DataFrame({'xData': x, 'yData': y}) 
+sns.regplot(x='xData', y='yData', data=df)
 plt.show()
