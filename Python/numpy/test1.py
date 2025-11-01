0f7822252cfff1692f64d772a279f898f5474906
cloutlee <clout_lee@hotmail.com.tw>
2025-11-01 17:29:13 +0800

    test

--- Python/numpy/test1.py
+++ Python/numpy/test1.py
@@ -2,10 +2,14 @@ import numpy as np
 
 tmplist = [1, 2, 3, 4, 5]
 arr = np.array(tmplist)
-print(arr)
-print(arr.shape)
-print(arr.dtype)
-print(type(arr))
-print(arr.ndim)
-print(arr.size)
+# print(arr)
+# print(arr.shape)
+# print(arr.dtype)
+# print(type(arr))
+# print(arr.ndim)
+# print(arr.size)
 
+print(tmplist[:])
+print(tmplist[1:3])
+print(tmplist[2:])
+print(tmplist[:3])
