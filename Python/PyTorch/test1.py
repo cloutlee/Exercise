cb9b25aecc7c6e5be9270a4007b89ebf3900a597
cloutlee <clout_lee@hotmail.com.tw>
2026-01-10 20:46:57 +0800

    test

--- Python/PyTorch/test1.py
+++ Python/PyTorch/test1.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+from torchvision import transforms
 
 
 # print(torch.zeros(5))
@@ -13,6 +14,23 @@ import torch
 # print(torch.randint(0, 10, (3, 3)))
 # print(torch.empty(3, 3))
 # print(torch.tensor([1.0, 2.0, 3.0]))
+
+
+
+
+# 將多個圖像預處理步驟（Transforms）串聯起來,定義一個組合操作
+my_transform = transforms.Compose([
+    transforms.Resize(256),              # 1. 將圖片短邊縮放至 256
+    transforms.CenterCrop(224),          # 2. 從中心裁剪出 224x224
+    transforms.ToTensor(),               # 3. 轉為 Tensor 並歸一化到 [0, 1]
+    transforms.Normalize(                # 4. 標準化（減均值、除以標準差）
+        mean=[0.485, 0.456, 0.406], 
+        std=[0.229, 0.224, 0.225]
+    )
+])
+# 使用時只需要調用一次
+# transformed_img = my_transform(img)
+
 
 
 
