af290d8aa671f04470852cc42a7d9c8e187c13da
cloutlee <clout_lee@hotmail.com.tw>
2026-01-10 21:12:32 +0800

    test

---
+++ Python/PyTorch/simplecnn.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchinfo import summary
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        # x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+batch_size = 64
+learning_rate = 0.001
+num_epochs = 2
+
+
+model = SimpleCNN()
+summary(model, input_size=(64, 1, 28, 28))
+
+
+
+# transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+# train_dataset = torchvision.datasets.MNIST(root='data', train=True, transform=transform, download=False)
+# train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+# model = SimpleCNN()
+
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# total_step = len(train_loader)
+# for epoch in range(num_epochs):
+#     for i, (images, labels) in enumerate(train_loader):
+#         # Forward pass        
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
+
+#         # Backward pass and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
+#         if (i + 1) % 100 == 0:
+#             print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item():.4f}')
+
+
